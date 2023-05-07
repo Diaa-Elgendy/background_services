0bff7558a8584939fa329f09012e476a6340720b
diaa-elgendy <diaaelgendy155@outlook.com>
2023-05-07 23:16:52 +0300

    Commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,7 @@
-import 'dart:async';
 import 'dart:math';
-
 import 'package:background_services/notification.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:location/location.dart';
 
@@ -17,18 +16,79 @@ class _HomeScreenState extends State<HomeScreen> {
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
   Location location = Location();
+  late bool serviceEnabled;
+  late PermissionStatus permissionGranted;
+  late LocationData placeLocation;
+  late LocationData myLocation;
 
   double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
     const p = 0.017453292519943295;
     final a = 0.5 -
         cos((lat2 - lat1) * p) / 2 +
         cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2;
-    return 12742 * asin(sqrt(a));
+    return (12742 * asin(sqrt(a))) * 1000;
+  }
+
+  Future<void> startLocationServices() async {
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    } else if (permissionGranted == PermissionStatus.deniedForever) {
+      //Todo: Alert to tell the user to open permission
+    } else {
+      myLocation = await location.getLocation();
+      location.changeSettings(accuracy: LocationAccuracy.balanced, interval: 50000);
+      location.enableBackgroundMode(enable: true);
+      print('Location Services Started');
+    }
+  }
+
+  void checkCurrentLocation() {
+    placeLocation = myLocation;
+    int ctr = 0;
+
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      // Use current location
+      ctr++;
+      double diff = calculateDistance(
+          myLocation.latitude!,
+          myLocation.longitude!,
+          placeLocation.latitude!,
+          placeLocation.longitude!);
+
+      if (diff > 100) {
+        //Todo: Check Out
+        location.enableBackgroundMode(enable: false);
+        print('Background mode Disabled');
+      }
+      print(
+          'Location Changed $ctr - long:${currentLocation.longitude} lat:${currentLocation.latitude}');
+      print('Location Changed $ctr - $diff M');
+      print('======================================================');
+      // Noti.showBigTextNotification(
+      //   title:
+      //   '$ctr long:${currentLocation.longitude} lat:${currentLocation.latitude}',
+      //   body: 'difference: $diff M',
+      //   fln: flutterLocalNotificationsPlugin,
+      // );
+    });
   }
 
   @override
   void initState() {
     super.initState();
+    startLocationServices().then((value) => checkCurrentLocation());
     Noti.initialize(flutterLocalNotificationsPlugin);
   }
 
@@ -41,62 +101,7 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           ElevatedButton(
             child: const Text('Get My Location'),
-            onPressed: () async {
-              bool serviceEnabled;
-              PermissionStatus permissionGranted;
-              LocationData placeLocation;
-              LocationData myLocation;
-
-              serviceEnabled = await location.serviceEnabled();
-              if (!serviceEnabled) {
-                serviceEnabled = await location.requestService();
-                if (!serviceEnabled) {
-                  return;
-                }
-              }
-
-              permissionGranted = await location.hasPermission();
-              if (permissionGranted == PermissionStatus.denied) {
-                permissionGranted = await location.requestPermission();
-                if (permissionGranted != PermissionStatus.granted) {
-                  return;
-                }
-              }
-
-              myLocation = await location.getLocation();
-              placeLocation = myLocation;
-              location.changeSettings(accuracy: LocationAccuracy.high);
-              location.enableBackgroundMode(enable: true);
-              int ctr = 0;
-
-              // var _distanceInMeters = await Geolocator.distanceBetween(
-              //   myLocation.latitude!,
-              //   myLocation.longitude!,
-              //   placeLocation.latitude!,
-              //   placeLocation.longitude!,
-              // );
-
-              // print('Distance: $_distanceInMeters');
-
-              location.onLocationChanged.listen((LocationData currentLocation) {
-                // Use current location
-                ctr++;
-                double diff = calculateDistance(
-                        myLocation.latitude!,
-                        myLocation.longitude!,
-                        placeLocation.latitude!,
-                        placeLocation.longitude!) *
-                    1000;
-                print(
-                    'Location Changed $ctr - long:${currentLocation.longitude} lat:${currentLocation.latitude}');
-                Noti.showBigTextNotification(
-                  title:
-                      '$ctr long:${currentLocation.longitude} lat:${currentLocation.latitude}',
-                  body: 'difference: $diff',
-                  fln: flutterLocalNotificationsPlugin,
-                );
-              });
-            },
+            onPressed: () async {},
           ),
           ElevatedButton(
             child: const Text('Dispose Location Services'),
